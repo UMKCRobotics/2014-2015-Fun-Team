440965395c7c933dc7b2f0012b8ba1563473af83
Chris Denniston <octopuscabbage@gmail.com>
2014-10-05 14:43:16 -0500

    made it compile

--- src/MotorController.cpp
+++ src/MotorController.cpp
@@ -1,42 +1,43 @@
 #include "MotorController.h"
 
-MotorController::MotorController(){
-	fd1 = DMCCStart(0);
-	fd2 = DMCCStart(1);
+FunMotorController::FunMotorController(){
+	fd1 = DMCCstart(0);
+	fd2 = DMCCstart(1);
 }
-MotorController::~MotorController(){
-	DMCCEnd(fd1);
-	DMCCEnd(fd2);
+FunMotorController::~FunMotorController(){
+	DMCCend(fd1);
+	DMCCend(fd2);
 }
 
-RobotState RobotState::move(RobotState state, Cardinal c){
+RobotState FunMotorController::move(RobotState state, Cardinal c){
 	RobotState s = state;
-	if(state->currentDirection != c){
+	if(state.currentDirection != c){
 	       	turn(state,c);	
-		s->currentDirection = c;
+		s.currentDirection = c;
 	}
 	switch(c){
 		case NORTH: 
-			s->currentNode -= 7;
+			s.currentNode -= 7;
 			break;
 		case SOUTH: 
-			s->currentNode += 7;
+			s.currentNode += 7;
 			break;
 		case EAST:
-			s->currentNode += 1;
+			s.currentNode += 1;
 			break;
 		case WEST:
-			s->currentNode -= 1;
+			s.currentNode -= 1;
 			break;
 	}
 	moveForwardOneSquare();
 	return s;
 }
-RobotState RobotState::turn(RobotState state, Cardinal c){
+RobotState FunMotorController::turn(RobotState state, Cardinal c){
 	RobotState s = state;
-	switch(state->currentDirection){
+	switch(state.currentDirection){
 		case NORTH:
 			switch(c){
+				case NORTH: break;
 				case SOUTH:
 					turnAround();
 					break;
@@ -49,6 +50,7 @@ RobotState RobotState::turn(RobotState state, Cardinal c){
 			} break;
 		case SOUTH:
 			switch(c){
+				case SOUTH: break;
 				case NORTH:
 					turnAround();
 					break;
@@ -61,6 +63,7 @@ RobotState RobotState::turn(RobotState state, Cardinal c){
 			} break;
 		case EAST:
 			switch(c){
+				case EAST: break;
 				case WEST:
 					turnAround();
 					break;
@@ -73,6 +76,7 @@ RobotState RobotState::turn(RobotState state, Cardinal c){
 			} break;
 		case WEST:
 			switch(c){
+				case WEST: break;
 				case EAST:
 					turnAround();
 					break;
@@ -84,6 +88,14 @@ RobotState RobotState::turn(RobotState state, Cardinal c){
 					break;
 			} break;
 	}
-	s->currentDirection = c;
+	s.currentDirection = c;
 	return s;
 }
+
+void FunMotorController::turnAround(){
+}
+void FunMotorController::turnLeft(){
+}
+void FunMotorController::turnRight(){
+}
+void FunMotorController::moveForwardOneSquare(){}
--- src/MotorController.h
+++ src/MotorController.h
@@ -1,18 +1,24 @@
 #include "DMCC.h"
 #include "Cardinal.h"
+#include "RobotState.h"
 
-class MotorController{
+#ifndef FUN_MOTOR_CONTROLLER_H
+#define FUN_MOTOR_CONTROLLER_H
+
+class FunMotorController{
+
 	private: 
 		int fd1;
 		int fd2;
 		void moveForwardOneSquare();
 		void turnLeft();
 		void turnRight();
-		viod turnAround();
+		void turnAround();
 	public:
-		MotorController();
-		~MotorController();
+		FunMotorController();
+		~FunMotorController();
 		RobotState turn(RobotState,Cardinal);
 		RobotState move(RobotState,Cardinal);
-}
+};
 
+#endif
