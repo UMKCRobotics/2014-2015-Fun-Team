fa1aa539bbe253c19f0119afb2e214865bb4e9bb
Chase Peterson <cvp21594@yahoo.com>
2014-10-17 20:22:17 -0500

    testing analog

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,20 @@
 #include "RobotState.h"
 #include "GPIOManager.h"
 #include "GPIOConst.h"
-#include <highgui.h>
 #include <iostream>
 
 int main(void){
 	Configuration* config = ConfigurationFactory::constructBlankConfig();
 	RobotState* robot = new RobotState(config);
 	GPIO::GPIOManager* gp = GPIO::GPIOManager::getInstance();
+  	int pin = GPIO::GPIOConst::getInstance()->getGpioByKey("AIN0");
+	std::cout << gp->getValue(pin);
+
 	gp->clean();
-	delete config;
-	delete robot;
+
 	delete gp;
 
 
-	cv::VideoCapture capture(0);
-	if (!capture.isOpened()) {
-		std::cerr << "Unable to open the camera" << std::endl;
-		return(10);
-	}
 	std::cout << "Congraturations, I compile!" << std::endl;
 	return 0;
 }
