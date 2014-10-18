d64084a8218a78f681aa78ee1cb7ee36223bf3fa
octopuscabbage <octopuscabbage@gmail.com>
2014-10-17 21:54:55 -0500

    changed to blacklib, should work a lot better

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,11 @@
 #include "Cardinal.h"
 #include "ConfigManager.h"
 #include "RobotState.h"
-#include "GPIOManager.h"
-#include "GPIOConst.h"
 #include <iostream>
 
 int main(void){
 	Configuration* config = ConfigurationFactory::constructBlankConfig();
 	RobotState* robot = new RobotState(config);
-	GPIO::GPIOManager* gp = GPIO::GPIOManager::getInstance();
-  	int pin = GPIO::GPIOConst::getInstance()->getGpioByKey("AIN0");
-	std::cout << gp->getValue(pin);
-
-	gp->clean();
-
-	delete gp;
-
 
 	std::cout << "Congraturations, I compile!" << std::endl;
 	return 0;
