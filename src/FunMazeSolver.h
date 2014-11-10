61f3524096e6739737853e65e3eec58052d7c5fe
octopuscabbage <octopuscabbage@gmail.com>
2014-11-10 12:13:31 -0600

    added documentation

--- src/FunMazeSolver.h
+++ src/FunMazeSolver.h
@@ -5,6 +5,11 @@ using std::set;
 #define FUN_MAZE_SOLVER_H
 
 namespace FunMazeSolver{
+	/*
+	 * Given the current state of the robot and a set of openings
+	 * return what direction the robot should turn to face.
+	 * Should always turn to the right.
+	 */
 	Cardinal computeOpenings(RobotState r,set<Cardinal> openings);
 }
 
