61ef7f84bc1c02dbf6e78d93bccda6a613b8116d
Caleb Chase <caleb@chasecaleb.com>
2014-11-11 20:55:45 -0600

    Stupid script is less stupid.

--- scripts/auto-rsync-and-build.sh
+++ scripts/auto-rsync-and-build.sh
@@ -7,11 +7,9 @@ LOCAL_DIR="/home/caleb/sync/code/2014-2015-Fun-Team"
 REMOTE_DIR="/root/caleb/"
 IP="root@192.168.7.2"
 
-inotifywait --monitor --recursive --event modify,close_write,move,create,delete $LOCAL_DIR |
-    while read file; do
+while inotifywait --recursive --event modify,close_write,move,create,delete $LOCAL_DIR; do
         echo "Copying..."
         rsync -av $LOCAL_DIR $IP:$REMOTE_DIR
-        ssh $IP "cd $REMOTE_DIR/2014-2015-Fun-Team"
         echo "Done"
         echo ""
-    done
+done
