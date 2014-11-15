e85b66d185d0a11cfd44b6b4c283552930674b74
Caleb Chase <caleb@chasecaleb.com>
2014-11-14 18:55:32 -0600

    Progress in RAM-based sensor usage

--- pru-assembly/main.cpp
+++ pru-assembly/main.cpp
@@ -8,8 +8,8 @@
 
 #define PRU_NUM 1
 
-uint8_t *getAddr();
-void readPins(uint8_t *addr, uint8_t values[32]);
+uint32_t *getAddr();
+void readPins(uint32_t *addr, uint32_t values[32]);
 
 int main () {
     /* Initialize structure used by prussdrv_pruintc_intc   */
@@ -23,35 +23,43 @@ int main () {
     prussdrv_pruintc_init(&pruss_intc_initdata);
 
     // Get address and write part of memory
-    uint8_t *pruData = getAddr();
+    uint32_t *pruData = getAddr();
 
     /* Load and execute binary on PRU */
     prussdrv_exec_program(PRU_NUM, "./prucode.bin");
 
     /* Wait for event completion from PRU */
     // There's a stupid bug where events are duplicated.
-    prussdrv_pru_wait_event(PRU_EVTOUT_1);
-    prussdrv_pru_clear_event(PRU_EVTOUT_1, PRU1_ARM_INTERRUPT);
-    prussdrv_pru_wait_event(PRU_EVTOUT_1);
-    prussdrv_pru_clear_event(PRU_EVTOUT_1, PRU1_ARM_INTERRUPT);
-    printf("input done\n");
+    /*
+     * prussdrv_pru_wait_event(PRU_EVTOUT_1);
+     * prussdrv_pru_clear_event(PRU_EVTOUT_1, PRU1_ARM_INTERRUPT);
+     * prussdrv_pru_wait_event(PRU_EVTOUT_1);
+     * prussdrv_pru_clear_event(PRU_EVTOUT_1, PRU1_ARM_INTERRUPT);
+     * printf("input done\n");
+     */
 
-    pruData[0] = 0;
-    pruData[1] = 0;
-    pruData[2] = 0;
-    pruData[3] = 0;
+    /*
+     * pruData[0] = 0;
+     * pruData[1] = 0;
+     * pruData[2] = 0;
+     * pruData[3] = 0;
+     */
 
-    for (int x = 0; x < 15; x++) {
-        uint8_t values[32];
-        readPins(pruData, values);
-        for (int i = 0; i < 32; i++) {
-            printf("%u ", values[i]);
-        }
-        printf("\n");
-        usleep(500000);
+    while (true) {
+        /*
+         * uint32_t values[32];
+         * readPins(pruData, values);
+         * for (int i = 0; i < 32; i++) {
+         *     printf("%u ", values[i]);
+         * }
+         */
+        printf("%u\n", pruData[0]);
+        usleep(250000);
     }
-    prussdrv_pru_wait_event(PRU_EVTOUT_1);
-    prussdrv_pru_clear_event(PRU_EVTOUT_1, PRU1_ARM_INTERRUPT);
+    /*
+     * prussdrv_pru_wait_event(PRU_EVTOUT_1);
+     * prussdrv_pru_clear_event(PRU_EVTOUT_1, PRU1_ARM_INTERRUPT);
+     */
 
     /* Disable PRU and close memory mappings */
     prussdrv_pru_disable(PRU_NUM);
@@ -60,17 +68,19 @@ int main () {
     return 0;
 }
 
-uint8_t *getAddr() {
+uint32_t *getAddr() {
     void *addr;
     prussdrv_map_prumem(PRUSS0_PRU1_DATARAM, &addr);
-    return (uint8_t *)addr;
+    return (uint32_t *)addr;
 }
 
 /*
  * Read values from PRU into array.
  */
-void readPins(uint8_t *addr, uint8_t values[8]) {
-    for (int i = 0; i < 32; i++) {
-        values[i] = (addr[i/8] >> (i % 8)) & 1;
-    }
-}
+/*
+ * void readPins(uint32_t *addr, uint32_t values[8]) {
+ *     for (int i = 0; i < 32; i++) {
+ *         values[i] = (addr[i/8] >> (i % 8)) & 1;
+ *     }
+ * }
+ */
--- pru-assembly/prucode.p
+++ pru-assembly/prucode.p
@@ -7,7 +7,7 @@
 // CHARGE: set all high, wait, set all low
 // Set outputs to high
 // Loop: wait 10 micros
-// Set outputs low
+// DON'T set low
 //
 // READ: (reset counts/loop), read pins, incr. each counter if high, loop until all low
 // Read inputs
@@ -22,46 +22,41 @@ INIT:
     CLR  r0, r0, 4
     SBCO r0, C4, 4, 4
 
-    gpio_dir_out P8_38, GPIO2_ADDR
 
-    MOV GPIO_ADDR_REG, GPIO2_ADDR | GPIO_DATAOUT_OFFSET
-    MOV LOOP_REG, 100000000
-
-LOOP_TOGGLE_OUT:
-    // Set high
-    set_gpio (1 << P8_38)
-
-    // Set low
-    set_gpio 0
-
-    // Loop
-    SUB LOOP_REG, LOOP_REG, 1
-    QBNE LOOP_TOGGLE_OUT, LOOP_REG, 0
-
-INIT_READ:
-    MOV R31.b0, PRU1_ARM_INTERRUPT+16
-
-    // Set pin to input.
-    MOV GPIO_ADDR_REG, GPIO2_ADDR | GPIO_OE_OFFSET
-    LBBO OE_REG, GPIO_ADDR_REG, 0, 4
-    SET OE_REG, P8_38
-    SBBO OE_REG, GPIO_ADDR_REG, 0, 4
-
-    MOV GPIO_ADDR_REG, GPIO2_ADDR | GPIO_DATAIN_OFFSET  // Store GPIO input address.
+    // MOV GPIO_ADDR_REG, GPIO2_ADDR | GPIO_DATAOUT_OFFSET
     MOV RAM_ADDR_REG, 0  // Store data ram address (0) for communication with host.
 
-    MOV LOOP_REG, 50000000  // Reset loop counter
+    // MOV LOOP_REG, 1337
+    // SBBO LOOP_REG, RAM_ADDR_REG, 0, 4
+
+    // gpio_dir_out P8_36, GPIO2_ADDR
+
+    // set_gpio (1 << P8_36)
+
+// CHARGE:
+    // set_gpio (1 << P8_36)  // Set high
+    // delay_micros 15  // Wait for capacitor to charge
+    // gpio_dir_in P8_36, GPIO2_ADDR  // Set to input
+
+gpio_dir_in P8_36, GPIO2_ADDR
+MOV GPIO_ADDR_REG, GPIO2_ADDR | GPIO_DATAIN_OFFSET  // Store GPIO input address.
+
+INIT_READ:
+    MOV LOOP_REG, 0  // For input time counting
+
+WAIT_HIGH:  // Temporary use for testing
+    LBBO GPIO_DATA_REG, GPIO_ADDR_REG, 0, 4
+    QBBC WAIT_HIGH,  GPIO_DATA_REG, P8_36
 
 LOOP_READ_IN:
+    ADD LOOP_REG, LOOP_REG, 1
     LBBO GPIO_DATA_REG, GPIO_ADDR_REG, 0, 4  // Read inputs into local register.
-    SBBO GPIO_DATA_REG, RAM_ADDR_REG, 0, 4  // Send to RAM for main CPU to read.
+    QBBS LOOP_READ_IN, GPIO_DATA_REG, P8_36
 
-    // Loop counter/jump
-    SUB LOOP_REG, LOOP_REG, 1
-    QBNE LOOP_READ_IN, LOOP_REG, 0
-
-    // Send event interrupt to main CPU and stop.
-    MOV R31.b0, PRU1_ARM_INTERRUPT+16
-    HALT
+    SBBO LOOP_REG, RAM_ADDR_REG, 0, 4
+    QBA INIT_READ
 
 
+// Send event interrupt to main CPU and stop.
+// MOV R31.b0, PRU1_ARM_INTERRUPT+16
+// HALT
