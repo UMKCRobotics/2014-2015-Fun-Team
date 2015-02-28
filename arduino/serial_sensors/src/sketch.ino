34da783c2fe31fb8cb830ae490378a53d759e48a
Chris Denniston <octopuscabbage@gmail.com>
2015-02-28 16:40:41 -0600

    Arduino serial code

--- arduino/serial_sensors/src/sketch.ino
+++ arduino/serial_sensors/src/sketch.ino
@@ -1,49 +1,48 @@
-#include <SoftwareSerial.h>
 #include <QTRSensors.h>
 
-// SYNC_PIN and serial tx are used for communication with Beaglebone PRU.
-// Important: serial rx is not physically connected since communication is one
-// way, but it is still reserved for the SoftwareSerial instance.
-#define SYNC_PIN 9
-SoftwareSerial bbSerial(7, 8);
-
-// 14 = left, 15 = center, 16 = right.
-unsigned char linePins[] = {14, 15, 16};
+/*
+ * PINS:
+ * Lines: D7 (left), A0 (center), D6 (right)
+ * Distance: A2 (front), A3 (front left), A1 (front right),
+ * A4 (back left), A5 (back right).
+ */
+unsigned char linePins[] = {7, 14, 6};
 QTRSensorsRC lineSensors(linePins, 3);
 
 void setup()
 {
     Serial.begin(115200);
-
-    bbSerial.begin(115200);
-    pinMode(SYNC_PIN, OUTPUT);
-    digitalWrite(SYNC_PIN, LOW);
 }
 
 void loop() {
-    uint16_t distanceValues[5];
-    // Read infrared/distance sensors (front = A3, back right = A7)
-    distanceValues[0] = analogRead(A3);
-    distanceValues[1] = analogRead(A4);
-    distanceValues[2] = analogRead(A5);
-    distanceValues[3] = analogRead(A6);
-    distanceValues[4] = analogRead(A7);
+    if (Serial.available() > 0) {
+	char cmd = Serial.read();
+        if (cmd == 'l') {
+	    // Read line sensors.
+	    uint16_t lineValues[3];
+	    lineSensors.read(lineValues);
 
-    // Read line sensors.
-    uint16_t lineValues[3];
-    lineSensors.read(lineValues);
+	    // Send
+	    Serial.write((uint8_t *)lineValues, 2*3);
+	    //for (int i = 0; i < 3; i++) {
+		//Serial.print(lineValues[i]);
+		//Serial.print(' ');
+	    //}
+        } else if (cmd == 'd') {
+	    // Read infrared/distance sensors
+	    uint16_t distanceValues[5];
+	    distanceValues[0] = analogRead(A2);
+	    distanceValues[1] = analogRead(A3);
+	    distanceValues[2] = analogRead(A1);
+	    distanceValues[3] = analogRead(A4);
+	    distanceValues[4] = analogRead(A5);
 
-    // Signal to PRU that next slice of readings are about to be transmitted.
-    digitalWrite(SYNC_PIN, HIGH);
-    digitalWrite(SYNC_PIN, LOW);
-
-    // Send values.
-    for (int i = 0; i < 5; i++) {
-        bbSerial.write(highByte(distanceValues[i]));
-        bbSerial.write(lowByte(distanceValues[i]));
-    }
-    for (int i = 0; i < 3; i++) {
-        bbSerial.write(highByte(lineValues[i]));
-        bbSerial.write(lowByte(lineValues[i]));
+	    // Send
+	    //for (int i = 0; i < 5; i++) {
+		//Serial.print(distanceValues[i]);
+		//Serial.print(' ');
+	    //}
+	    Serial.write((uint8_t *)distanceValues, 2*5);
+	}
     }
 }
